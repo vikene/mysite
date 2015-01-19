1ed005ea5257092b4159d2809e3080d56d1f17b2
Vigneash Sundar <vigneashsundar@live.com>
2015-01-19 11:14:20 +0530

    blog render code added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ var app = require('express');
 var router = app.Router();
 
 router.get('/',function(req,res){
-res.render('index',{title:"Vikene's Blog",status:"Offline"});
+    
+    var db = req.db;
+    var collection = db.get('blog');
+    collection.find({},function(err,doc){
+        var buffer = doc.length;
+    
+        for(var i =0;i<doc.length;i++)
+        {
+            buffer[doc.length-1] = doc[i];
+        }
+    
+    })
+res.render('index',{title:"Vikene's Blog",status:"Offline",bloglist:buffer});
 
 })
 
